dc6b58e3c8a24cc7a6075945314ac1a7943aa7cd
SanderMertens <sander.mertens8@gmail.com>
2015-04-14 18:25:34 -0400

    Updated subscriber example

--- examples/helloworld/sub.cpp
+++ examples/helloworld/sub.cpp
@@ -2,6 +2,7 @@
 #include "Hello_DCPS.h"
 
 class HelloWorldHandler : dds::mq::Handler<Hello::World> {
+    HelloWorldHandler() {}
     void on_message(Hello::World sample) {
         std::cout << "Message received: " << sample.value();
     }
@@ -11,7 +12,7 @@ int main(int argc, char *arv[])
 {
     dds::domain::DomainParticipant dp( 0 );
     dds::mq::Subscriber sub( dp, "myqueue" );
-    dds::mq::Reader<Hello::World> reader( sub );
+    dds::mq::Reader<Hello::World> reader( sub, HelloWorldHandler() );
 
     while(1) {
         sub->dispatch();
